ea394f07dc1d28fd32772fa158ddc11a7f1d31e8
Rotem Yaari <vmalloc@gmail.com>
2019-10-24 13:32:44 +0300

    Reproduce bug 18515

--- app/router.js
+++ app/router.js
@@ -1,12 +1,14 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
 Router.map(function() {
+  this.route("not_found", { path: "/*:unknown" });
+  this.route("item", { path: "/items/:item_id" });
 });
 
 export default Router;
---
+++ app/routes/application.js
@@ -0,0 +1,27 @@
+import { later } from "@ember/runloop";
+import { inject as service } from "@ember/service";
+import Route from "@ember/routing/route";
+
+export default Route.extend({
+  router: service(),
+  actions: {
+    error(err, transition) {
+      let router = this.get("router");
+      if (err.errors && err.errors[0].status === "404") {
+        transition.abort();
+        return later(function() {
+          let transition_name = transition.to.name;
+          if (transition_name.endsWith(".index")) {
+            transition_name = "secure.customer";
+          }
+          let url = router.urlFor(transition_name, transition.to.params);
+          // Workaround for a bug in ember that causes the router to fail if the url starts with a slash
+          if (url.startsWith("/")) {
+            url = url.substr(1);
+          }
+          return router.replaceWith("not_found", url);
+        });
+      }
+    },
+  },
+});
